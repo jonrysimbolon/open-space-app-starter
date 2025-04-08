5cbe5403fb374ef2bc6133444cd1714ddc4dc707
jonry <jonrysimbolon19@gmail.com>
2025-04-08 09:53:28 +0700

    first commit

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,44 @@
+import React, { useEffect } from 'react';
+import TalkInput from '../components/TalkInput';
+import TalksList from '../components/TalksList';
+import { asyncPopulateUsersAndTalks } from '../states/shared/action';
+import { asyncAddTalk, asyncToogleLikeTalk } from '../states/talks/action';
+import { useDispatch, useSelector } from 'react-redux';
+
+function HomePage() {
+  const {
+    talks = [],
+    users = [],
+    authUser,
+  } = useSelector((states) => states);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(asyncPopulateUsersAndTalks());
+  }, [dispatch]);
+
+  const onAddTalk = (text) => {
+    // @TODO: dispatch async action to add talk
+
+  };
+
+  const onLike = (id) => {
+    // @TODO: dispatch async action to toggle like talk
+  };
+
+  const talkList = talks.map((talk) => ({
+    ...talk,
+    user: users.find((user) => user.id === talk.user),
+    authUser: authUser.id,
+  }));
+
+  return (
+    <section className="home-page">
+      <TalkInput addTalk={onAddTalk} />
+      <TalksList talks={talkList} like={onLike} />
+    </section>
+  );
+}
+
+export default HomePage;
---
+++ src/states/index.js
@@ -0,0 +1,18 @@
+import { configureStore } from "@reduxjs/toolkit";
+import authUserReducer from './authUser/reducer';
+import isPreloadReducer from './isPreload/reducer';
+import talkDetailReducer from './talkDetail/reducer';
+import talksReducer from './talks/reducer';
+import usersReducer from './users/reducer';
+
+const store = configureStore({
+  reducer: {
+    authUser: authUserReducer,
+    isPreload: isPreloadReducer,
+    users: usersReducer,
+    talks: talksReducer,
+    talkDetail: talkDetailReducer,
+  },
+});
+
+export default store;
